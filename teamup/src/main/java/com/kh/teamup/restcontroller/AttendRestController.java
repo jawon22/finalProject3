58785533cec659c92e4fde96fe416a9ba9f5ff45
newstar95 <byeol12120@gmail.com>
2023-11-08 18:06:53 +0900

    근태관리 테이블 DB 구현, 근태관리 CRU 일부 구현

---
+++ teamup/src/main/java/com/kh/teamup/restcontroller/AttendRestController.java
@@ -0,0 +1,16 @@
+package com.kh.teamup.restcontroller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/attend")
+public class AttendRestController {
+	// @GetMapping - 조회 (post도 가능)
+	// @PostMapping- - 등록
+	// @PutMapping - 전체 수정
+	// @PatchMapping - 일부 수정
+	// @DeleteMapping - 삭제
+}
