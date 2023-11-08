34489b5273e7c7257160d6a7d4ec5bfd32ef49ba
Bokman0201 <jeonghs24@nate.com>
2023-11-08 17:42:43 +0900

    수정기능 구현중

--- teamup/src/main/java/com/kh/teamup/dao/EmpDaoImpl.java
+++ teamup/src/main/java/com/kh/teamup/dao/EmpDaoImpl.java
@@ -1,5 +1,8 @@
 package com.kh.teamup.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -13,4 +16,36 @@ public class EmpDaoImpl implements EmpDao {
 	public void addEmp(EmpDto empDto) {
 		sqlSession.insert("emp.addEmp",empDto);
 	}
+	
+	
+	
+	
+	
+	//메일 전송할때 실횅할코드 아이디 생성 이거랑 비밀번호 랜던? 아니면 초기 비밀번호 생성으로 해서 사용
+	@Override
+	public void updateEmpId(int empNo, EmpDto empDto) {
+		Map<String, Object> params = Map.of("empNo",empNo ,"empDto",empDto);
+		//int result =sqlSession.update("emp.updateEmpId",params);
+		sqlSession.update("emp.updateEmpId",params);
+		//if(result==0) throw new NotargetException();
+	}
+	
+	@Override
+	public List<EmpDto> empList() {
+		
+		return sqlSession.selectList("emp.empList");
+	}
+	
+	@Override
+	public void deleteEmp(int empNo) {
+		sqlSession.delete("emp.deleteEmp",empNo);
+	}
+	
+	@Override
+	public void empInfoUpdate(int empNo, EmpDto empDto) {
+		Map<String, Object> params = Map.of("empNo",empNo ,"empDto",empDto);
+		//int result =sqlSession.update("emp.updateEmpId",params);
+		sqlSession.update("emp.empInfoUpdate",params);		
+	}
+	
 }
