58a80873f96ee9b68db1423964cef4a3f2cad16a
jawon22 <winwkdnjs@naver.com>
2023-11-08 11:15:44 +0900

    파이널 프로젝트 생성

---
+++ teamup/src/test/java/com/kh/teamup/TeamupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.teamup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
