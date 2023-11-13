629d16427b0e0f3f65e9d4516ad21d8f3ea6d2fc
Bokman0201 <jeonghs24@nate.com>
2023-11-13 22:54:27 +0900

    부서 컨트롤러 수정

--- teamup/src/main/java/com/kh/teamup/dao/DeptDaoImpl.java
+++ teamup/src/main/java/com/kh/teamup/dao/DeptDaoImpl.java
@@ -1,10 +1,15 @@
 package com.kh.teamup.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.teamup.dto.DeptDto;
+import com.kh.teamup.vo.DeptVo;
 @Repository
 public class DeptDaoImpl implements DeptDao {
 	@Autowired
@@ -13,5 +18,19 @@ public class DeptDaoImpl implements DeptDao {
 	@Override
 	public void addDept(DeptDto deptDto) {
 		sqlSession.insert("dept.addDept",deptDto);
+	}
+	
+	
+	@Override
+	public List<DeptVo> deptList(String comId) {
+		return sqlSession.selectList("dept.listByCompany",comId);
+	}
+	
+	@Override
+	public void deptUpdate(int deptNo ,DeptDto deptDto) {
+		Map<String, Object> params =Map.of("deptNo",deptNo,"deptDto",deptDto);
+	
+		
+		sqlSession.update("dept.update", params);
 	}
 }
--- teamup/src/main/java/com/kh/teamup/restcontroller/DeptRestController.java
+++ teamup/src/main/java/com/kh/teamup/restcontroller/DeptRestController.java
@@ -1,15 +1,22 @@
 package com.kh.teamup.restcontroller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.teamup.dao.DeptDao;
 import com.kh.teamup.dto.DeptDto;
+import com.kh.teamup.vo.DeptVo;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "부서관리" , description = "부서CRUD")
 @RestController
@@ -22,6 +29,18 @@ public class DeptRestController {
 	@PostMapping("/")
 	public void addDept(@RequestBody DeptDto deptDtp) {
 		deptDao.addDept(deptDtp);
+	}
+	@Operation(description = "회사별 부서 부서원의 수 ")
+	@GetMapping("/listByCompany/{comId}")
+	public List<DeptVo> listByCompany(@PathVariable String comId){
+		return deptDao.deptList(comId);
+		
+	}
+	
+	@PutMapping("/update/{deptNo}")
+	public void update( @PathVariable int deptNo ,@RequestBody DeptDto deptDto) {
+		deptDao.deptUpdate(deptNo,deptDto);
+		
 	}
 
 }
