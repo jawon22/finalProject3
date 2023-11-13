e8413656c98dc9e5bc19a5176181bac848733f65
jawon22 <winwkdnjs@naver.com>
2023-11-13 23:16:42 +0900

    전자결재 결재선 구현중( mapper, dao)

--- teamup/src/main/java/com/kh/teamup/dao/ApprovePathDaoImpl.java
+++ teamup/src/main/java/com/kh/teamup/dao/ApprovePathDaoImpl.java
@@ -1,5 +1,7 @@
 package com.kh.teamup.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -22,5 +24,9 @@ public class ApprovePathDaoImpl implements ApprovePathDao{
 		sqlSession.insert("approvePath.approvePathSave",approvePathDto);
 	}
 	
+	@Override
+	public List<ApprovePathDto> selectByApprNo(int apprNo) {
+		return sqlSession.selectList("approvePath.findByApprNo", apprNo);
+	}
 	
 }
