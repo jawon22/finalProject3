c80764a7371b2c8aac81aaf6e231da96f50b4145
seonmikwak <sunmi1220ks@naver.com>
2023-11-13 21:12:08 +0900

    프로필 찍엇다아아악!

--- teamup/src/main/java/com/kh/teamup/restcontroller/ProfileRestController.java
+++ teamup/src/main/java/com/kh/teamup/restcontroller/ProfileRestController.java
@@ -2,14 +2,18 @@ package com.kh.teamup.restcontroller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +23,7 @@ import com.kh.teamup.dao.ProfileDao;
 import com.kh.teamup.dto.AttachDto;
 import com.kh.teamup.dto.ProfileDto;
 import com.kh.teamup.vo.ProfileImageVO;
+import com.kh.teamup.vo.ProfileInfoVO;
 import com.kh.teamup.vo.ProfileUpdateVO;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -119,6 +124,11 @@ public class ProfileRestController {
 		
 		return ResponseEntity.ok().body("프로필 이미지 수정성공");
 	}
+	
+	@GetMapping("/{empNo}")
+	public List<ProfileInfoVO> list(@PathVariable int empNo){
+		return profileDao.selectList(empNo);
+	} 
 	
 
 }
