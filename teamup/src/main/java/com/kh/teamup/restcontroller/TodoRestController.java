86ec39e7844ae71bca36bc5968e023c444e2caea
smilesyk0323 <smilesyk0323@naver.com>
2023-11-17 10:43:42 +0900

    todo crud 기능 구현

--- teamup/src/main/java/com/kh/teamup/restcontroller/TodoRestController.java
+++ teamup/src/main/java/com/kh/teamup/restcontroller/TodoRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,10 +51,10 @@ public class TodoRestController {
 		todoDao.deleteTodo(empNo);
 	}
 	
-//	@PutMapping("/{todoNo}")
-//	public void update(@PathVariable int todoNo) {
-//		todoDao.change(empNo, todoDto, todoNo);
-//	}
+	@PutMapping("/{todoNo}")
+	public void update(@RequestBody TodoDto todoDto, @PathVariable int todoNo) {
+		todoDao.change(todoDto, todoNo);
+	}
 
 }
 
