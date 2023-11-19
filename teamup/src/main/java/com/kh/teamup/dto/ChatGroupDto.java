f0d3d93599f03ae6b44c2cbed6c8148390b960d5
Bokman0201 <jeonghs24@nate.com>
2023-11-19 16:29:43 +0900

    채팅 시작

---
+++ teamup/src/main/java/com/kh/teamup/dto/ChatGroupDto.java
@@ -0,0 +1,13 @@
+package com.kh.teamup.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class ChatGroupDto {
+	private String chatMember;
+	private String chatRoomNo;
+
+}
---
+++ teamup/src/main/java/com/kh/teamup/dto/MessageDto.java
@@ -0,0 +1,16 @@
+package com.kh.teamup.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class MessageDto {
+	private int messageNo;
+	private int chatRoomNo;
+	private String chatContent;
+	private String messageTime;
+	private String messageStatus;
+	
+}
