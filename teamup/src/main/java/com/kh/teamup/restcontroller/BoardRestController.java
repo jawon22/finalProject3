024094931c9166b945a88c3f4a4912804cf48240
smilesyk0323 <smilesyk0323@naver.com>
2023-11-21 18:19:58 +0900

    공지사항 백 수정

--- teamup/src/main/java/com/kh/teamup/restcontroller/BoardRestController.java
+++ teamup/src/main/java/com/kh/teamup/restcontroller/BoardRestController.java
@@ -32,6 +32,7 @@ public class BoardRestController {
 	@Operation(description = "공지사항 등록")
 	@PostMapping("/add")
 	public void insert(@RequestBody BoardDto boardDto) {
+		log.debug("boardDto = {}", boardDto);
 		boardDao.insert(boardDto);
 	}
 	
